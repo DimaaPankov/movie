68eb61b4bdd4f7a59f018405e65ec2f5c2f72744
DimaPankov <dimaray100500@gmail.com>
2022-06-05 17:11:14 +0700

    1.0

---
+++ app/src/main/java/com/sekvenie/model/network/dataimpl/DataImpl.kt
@@ -0,0 +1,26 @@
+package com.sekvenie.model.network.dataimpl
+
+//import com.sekvenie.model.network.Api.retrofitService
+import com.sekvenie.model.network.service.Data
+import com.sekvenie.mainpresemter.DataApi
+import com.sekvenie.model.network.ApiService
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+class DataImpl : DataApi<Data> {
+    private var data: Data? = null
+
+    override suspend fun getData(): Data {
+        data = Data(listOf())
+        coroutineScope {
+            launch {
+                try {
+                    data = ApiService.getInstance().getPhotos()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        return data!!
+    }
+}
